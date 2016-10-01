b9ae94956a4c2158886052a6b8f2395e2e9e9b63
Craig M. Brandenburg <c.m.brandenburg@gmail.com>
2016-10-01 05:46:10 -0700

    Add documentation for v0.3.0

---
+++ doc/v0.3.0/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["chill"] = ["impl <a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='chill/struct.ViewFunction.html' title='chill::ViewFunction'>ViewFunction</a>","impl <a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='chill/struct.Design.html' title='chill::Design'>Design</a>","impl <a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='chill/struct.Revision.html' title='chill::Revision'>Revision</a>","impl <a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='https://hyperium.github.io/mime.rs/mime/struct.Mime.html' title='mime::Mime'>Mime</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(</a><a class='enum' href='https://hyperium.github.io/mime.rs/mime/enum.Attr.html' title='mime::Attr'>Attr</a>, <a class='enum' href='https://hyperium.github.io/mime.rs/mime/enum.Value.html' title='mime::Value'>Value</a><a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>)</a>&gt;&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.3.0/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["chill"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='chill/enum.Error.html' title='chill::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='https://hyperium.github.io/hyper/hyper/error/enum.Error.html' title='hyper::error::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
